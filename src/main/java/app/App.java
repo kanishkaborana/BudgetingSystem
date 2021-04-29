fd9bdc845fd26a254f457640ba2b6a3b60bc5bd8
Elise Merritt <em575@rutgers.edu>
2021-04-29 14:36:07 -0400

    Added comments

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,3 +1,7 @@
+/**
+Runs the budgeting system so it can be used
+@author Szymon Gogolowski, Kanishka Borana, ELise Merritt, Sushanth Nadam, Jacob Batista
+*/
 package app;
 
 import org.springframework.boot.SpringApplication;
@@ -5,7 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
-
+	
+  	/**
+    Main method that runs the budgeting system by calling the application's
+    run method
+    */
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 		
