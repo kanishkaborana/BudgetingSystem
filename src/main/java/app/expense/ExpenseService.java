c5d317cd74047d5f6dec4e5404ec2d8d9071b328
Elise Merritt <em575@rutgers.edu>
2021-04-29 11:10:09 -0400

    Added comments

--- src/main/java/app/expense/ExpenseService.java
+++ src/main/java/app/expense/ExpenseService.java
@@ -1,3 +1,7 @@
+/**
+Provides methods to manage a user's expenses
+@author Szymon Gogolowski, Kanishka Borana, ELise Merritt, Sushanth Nadam, Jacob Batista
+*/
 package app.expense;
 
 import java.sql.Date;
@@ -13,11 +17,20 @@ public class ExpenseService {
 
     @Autowired
     private ExpenseRepository expenseRepo;
-
+	
+  	/**
+    Retrieves all expenses in the budgeting system
+    @return all expenses fond in the budgeting system
+    */
     public Iterable<Expense> getAllExpense() {
 		return expenseRepo.findAll();
 	}
 	
+  	/**
+    Retrieves an expense
+    @param id the identification number of the expense to retrieve
+    @return myExpense expense with the given id
+    */
 	public Expense getExpenseByID(Integer id) {
 		try {
 			Expense myExpense = expenseRepo.findById(id).get();
@@ -28,24 +41,47 @@ public class ExpenseService {
 		}
 	}
 	
-
+	/**
+    Retrieves all expenses of a specified user
+    @param userID the identification number of the user whose expenses are
+    to be retrieved
+    @return all expenses of the user with the given id
+    */
 	public Iterable<Expense> getExpensesByUserID(String userID) {
 		return expenseRepo.getExpensesByUserID(userID);
 	}
-
+	
+  	/**
+    Adds an expense
+    @param newExpense the expense to add
+    */
 	public void addExpense(Expense expense) {
 		expenseRepo.save(expense);
 		
 	}
 	
+  	/**
+    Deletes an expense
+    @param expense the expense to delete
+    */
 	public void deleteExpense(Expense expense) {
 		expenseRepo.delete(expense);
 	}
 	
+  	/**
+    Deletes the expense with the given identification number
+    @param id the identification number of the expense to delete
+    */
 	public void deleteExpenseById(Integer id) {
 		expenseRepo.deleteById(id);
 	}
 	
+  	/**
+    Adds an expense after checking if the expense already exists
+    @param expense the expense to add
+    @return a message telling the user if the expense already exists or
+    has just been created.
+    */
     public String add(Expense expense) {
 		if (!expenseRepo.findById(expense.getExpenseID()).isPresent()) {
 			expenseRepo.save(expense);
@@ -57,6 +93,11 @@ public class ExpenseService {
 		
 	}
 	
+  	/**
+    Updates an expense
+    @param expense the expense to update
+    @return a message telling the user the expense was updated
+    */
 	public String update(Expense expense) {
 		Expense updateExpense = expenseRepo.findById(expense.getExpenseID()).get();
 		updateExpense.setExpenseTitle(expense.getExpenseTitle());
@@ -68,20 +109,38 @@ public class ExpenseService {
 		expenseRepo.save(updateExpense);
 		return "Expense updated";
 	}
-
+	
+  	/**
+    Retrieves all of a user's expenses in a selected month
+    @param id the id of the user whose expenses are to be retrieved
+    @param year the year the month to retrieve expenses from was in
+    @param month the month to retrieve expenses from 
+    @return all of a user's expenses for the given month
+    */
 	public Iterable<Expense> getExpensesByMonth(String id, int month, int year) {
 		Date date1 = new Date(year - 1900, month, 1);
 		Date date2 = new Date(year - 1900, month + 1, 0);
 		return expenseRepo.getExpensesBetween(id, date1, date2);
 	}
 
-
+	/**
+    Retrieves all of a user's expenses in a selected year
+    @param id the id of the user whose expenses are to be retrieved
+    @param year the year to retrieve expenses from 
+    @return all of a user's expenses for the given year
+    */
 	public Iterable<Expense> getExpensesByYear(String id, int year) {
 		Date date1 = new Date(year - 1900, 0, 1);
 		Date date2 = new Date(year - 1900, 11, 31);
 		return expenseRepo.getExpensesBetween(id, date1, date2);
 	}
-
+	
+  	/**
+    Retrieves all of a user's expenses in each month of a year, in the format of a monthly report 
+    @param id the id of the user whose expenses are to be retrieved
+    @param year the year to retrieve expenses from 
+    @return all of a user's expenses for the given year, broken down by month
+    */
 	public List<MonthlyReport> getMonthlyExpenses(String id, int year) {
 		String[] monthNames = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 		List<MonthlyReport> output = new ArrayList<MonthlyReport>();
