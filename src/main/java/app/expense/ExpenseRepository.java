8c7b659bdd251ffdb8e2e199e2f158de87250b96
Elise Merritt <em575@rutgers.edu>
2021-04-29 10:51:18 -0400

    Added comments

--- src/main/java/app/expense/ExpenseRepository.java
+++ src/main/java/app/expense/ExpenseRepository.java
@@ -1,3 +1,7 @@
+/**
+An interface for methods that use an expense repository
+@author Szymon Gogolowski, Kanishka Borana, Elise Merritt, Sushanth Nadam, Jacob Batista
+*/
 package app.expense;
 
 import java.sql.Date;
@@ -6,9 +10,22 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ExpenseRepository extends CrudRepository<Expense, Integer> {
+    
+    /**
+    Retrieves all expenses of a specified user
+    @param userID the identification number of the user whose expenses are
+    to be retrieved
+    */
     @Query(value = "SELECT * FROM budgeting_system.expense WHERE userID = (:userID)", nativeQuery = true)
 	public Iterable<Expense> getExpensesByUserID(String userID);
     
+    /**
+    Retrieves all expenses a user added between two dates
+    @param userID the identification number of the user whose expenses are
+    to be retrieved
+    @param date1 the earliest date to retrieve expenses from
+    @param date2 the latest date to retrieve expenses from
+    */
     @Query(value = "SELECT * FROM budgeting_system.expense WHERE (date_added between (:date1) and (:date2)) AND userID = (:userID) ", nativeQuery = true)
 	public Iterable<Expense> getExpensesBetween(String userID, Date date1, Date date2);
 }
